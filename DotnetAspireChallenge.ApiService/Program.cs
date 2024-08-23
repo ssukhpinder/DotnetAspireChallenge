ae3add33a1484d84de49f5be4a48708ec3682727
Sukhpinder Singh <sukhpinder.s.dhanjal@gmail.com>
2024-08-23 20:06:35 +0530

    Add aspire component for mssql with EF core

--- DotnetAspireChallenge.ApiService/Program.cs
+++ DotnetAspireChallenge.ApiService/Program.cs
@@ -1,16 +1,20 @@
 
 
 using Confluent.Kafka;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddKafkaProducer<string, string>("messaging");
+
+builder.AddSqlServerDbContext<MssqlDbContext>("sqldata");
 
 // Add service defaults & Aspire components.
 builder.AddServiceDefaults();
 
 // Add services to the container.
 builder.Services.AddProblemDetails();
-
-builder.AddKafkaProducer<string, string>("messaging");
 
 var app = builder.Build();
 
@@ -51,11 +55,55 @@ app.MapGet("/send", async (IProducer<string, string> services, string key, strin
 
 });
 
+app.MapGet("/mssql", async (MssqlDbContext mssqlDbContext) =>
+{
+
+    await mssqlDbContext.Customers.AddAsync(new Customer()
+    {
+        Title = "test@gmail.com",
+        Description = "sukh"
+    });
+    int rows = await mssqlDbContext.SaveChangesAsync();
+    if (rows > 0)
+    {
+        return await mssqlDbContext.Customers.FirstOrDefaultAsync();
+    }
+    else
+    {
+        return null;
+    }
+});
+
 app.MapDefaultEndpoints();
+if (app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var context = scope.ServiceProvider.GetRequiredService<MssqlDbContext>();
+        context.Database.EnsureCreated();
+    }
+}
 
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+internal class MssqlDbContext(DbContextOptions options) : DbContext(options)
+{
+
+    public DbSet<Customer> Customers => Set<Customer>();
+}
+
+public class Customer
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    public string Description { get; set; } = string.Empty;
 }
--- DotnetAspireChallenge.AppHost/Program.cs
+++ DotnetAspireChallenge.AppHost/Program.cs
@@ -7,7 +7,14 @@ var cache = builder.AddRedis("cache");
 var messaging = builder.AddKafka("messaging")
                        .WithKafkaUI();
 
-var apiService = builder.AddProject<Projects.DotnetAspireChallenge_ApiService>("apiservice").WithReference(messaging);
+
+var sql = builder.AddSqlServer("sql")
+                 .AddDatabase("sqldata");
+
+
+var apiService = builder.AddProject<Projects.DotnetAspireChallenge_ApiService>("apiservice")
+    .WithReference(messaging)
+    .WithReference(sql);
 
 
 
